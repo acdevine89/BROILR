ed4fe63bdf2c1f50a5e988e7c39ea6ee99e7e013
Matt Lauer <mattlauer@Administrators-MacBook-Air-2.local>
2014-10-23 16:51:59 -0400

    Updating .gitignore to ignore .txt files. Adding lots of pseudocode to FoodProfileStacker to describe how to read in JSON data and put it in food profiles.

--- app/src/main/java/com/example/broilr/FoodProfileStacker.java
+++ app/src/main/java/com/example/broilr/FoodProfileStacker.java
@@ -1,6 +1,9 @@
 package com.example.broilr;
 
 import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
 
 import java.io.BufferedReader;
 import java.net.HttpURLConnection;
@@ -17,6 +20,8 @@ public class FoodProfileStacker {
 
     public class FetchFoodProfileTask extends AsyncTask<Void, Void, Stack<FoodProfile>> {
 
+        private final String LOG_TAG = FetchFoodProfileTask.class.getSimpleName();
+
         @Override
         protected Stack<FoodProfile> doInBackground(Void... params) {
             // These two need to be declared outside the try/catch
@@ -25,16 +30,50 @@ public class FoodProfileStacker {
             BufferedReader reader = null;
             // Will contain the raw JSON response as a string.
             String foodProfileJsonString = null;
-
-
-
-            return null;
-        }
-
-        private Stack<FoodProfile> getFoodProfilesFromJson(String foodProfileJsonString) {
             Stack<FoodProfile> foodProfileStack;
 
-            return null; // Replace null later
+            // DO THE WORK OF SETTING UP A URL CONNECTION, see ForecastFragment.java in Sunshine
+            // Inludes try, catch, and finally for IOException
+            // If this code works, it fills foodProfileJsonString with a string represenation
+            // of ALL our JSON data.
+
+            // Now do a try, catch on getting meaningful data from JSON,
+            // and setting it to attributes of FoodProfiles in a stack.
+            try {
+                foodProfileStack = getFoodProfilesFromJson(foodProfileJsonString);
+            }
+            catch (JSONException e)  {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            }
+
+            return foodProfileStack;
+        }
+
+        private Stack<FoodProfile> getFoodProfilesFromJson(String foodProfileJsonString)
+                throws JSONException {
+
+            // These are the names of the JSON objects that need to be extracted.
+            final String API_BROILR_PROFILEID = "profileID";
+            final String API_BROILR_IMGURL = "imgURL";
+            final String API_BROILR_NAME = "name";
+            final String API_BROILR_AGE = "age";
+            final String API_BROILR_LASTACTIVE = "lastActive";
+            final String API_BROILR_BIO = "bio";
+
+            // 1. Turn foodProfileJsonString to JSONArray
+            // WHY?!?! Because JSONArrays and JSONObjects have tools built in to help us find data
+            // by the keys we described above.
+
+            // Create an empty stack of food profiles that we'll fill.
+            Stack<FoodProfile> foodProfileStack;
+            // 2. Iterate the JSONArray to find a JSONObject at each index,
+            //    then start filling FoodProfile objects with the data you find.
+            //    The format in pseudocode is JSONObject foodProfileObject = JSONArray.getJSONObject(i);
+            //    data = foodProfileObject.getString(KEY);
+            //    FoodProfile.setData(data);
+
+            return null; // Replace null later with foodProfileStack
         }
 
         @Override
