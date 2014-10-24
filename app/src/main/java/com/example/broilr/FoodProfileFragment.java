96c5fd89e4cff970d8c2745cc1e0c81d6d4bdb2c
acdevine89 <annie.devine@detroitlabs.com>
2014-10-24 16:24:41 -0400

    Created image views and text views within FoodProfileFragment.java, created a view inflater, created method to take each food profile off the stack and input its data into the layout views

--- app/src/main/java/com/example/broilr/FoodProfileFragment.java
+++ app/src/main/java/com/example/broilr/FoodProfileFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -16,6 +18,8 @@ import java.util.Stack;
 public class FoodProfileFragment extends Fragment implements OnFoodProfileStackReadyListener{
 
     FoodProfileStacker stacker = new FoodProfileStacker(this);
+    ImageView foodProfileImage;
+    TextView foodProfileText1, foodProfileText2, foodProfileText3, foodProfileText4;
 
     public FoodProfileFragment() {
 
@@ -29,7 +33,14 @@ public class FoodProfileFragment extends Fragment implements OnFoodProfileStackR
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return null; // Swap out with a view later
+        View view = inflater.inflate(R.layout.foodprofilefragment, container, false);
+        foodProfileImage = (ImageView) view.findViewById(R.id.img_url);
+        foodProfileText1 = (TextView) view.findViewById(R.id.name);
+        foodProfileText2 = (TextView) view.findViewById(R.id.food_age);
+        foodProfileText3 = (TextView) view.findViewById(R.id.last_active);
+        foodProfileText4 = (TextView) view.findViewById(R.id.food_bio);
+
+        return view; // Swap out with a view later
     }
 
     @Override
@@ -40,8 +51,21 @@ public class FoodProfileFragment extends Fragment implements OnFoodProfileStackR
 
     @Override
     public void foodProfilesAreReady(Stack<FoodProfile> foodProfileStack) {
+        FoodProfile thisFoodProfile = foodProfileStack.pop();
 
         String imgURL = thisFoodProfile.getImgURL();
-        Picasso.with(context).load(imgURL).into(imageView);
+        Picasso.with(getActivity()).load(imgURL).into(foodProfileImage);
+
+        String name = thisFoodProfile.getName();
+        foodProfileText1.setText(name);
+
+        String food_age = thisFoodProfile.getAge();
+        foodProfileText2.setText(food_age);
+
+        String last_active = thisFoodProfile.getLastActive();
+        foodProfileText3.setText(last_active);
+
+        String food_bio = thisFoodProfile.getBio();
+        foodProfileText4.setText(food_bio);
     }
 }
