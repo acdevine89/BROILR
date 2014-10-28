9d1a6684e1a3e0dc1bdee09e1aed5a0a8964e725
acdevine89 <annie.devine@detroitlabs.com>
2014-10-28 15:22:40 -0400

    Commenting out all gif things

--- app/src/main/java/com/example/broilr/GifWebView.java
+++ app/src/main/java/com/example/broilr/GifWebView.java
@@ -1,17 +1,17 @@
-package com.example.broilr;
-
-import android.content.Context;
-import android.webkit.WebView;
-
-/**
- * Created by anniedevine on 10/28/14.
- */
-
-public class GifWebView extends WebView {
-
-    public GifWebView(Context context, String path) {
-        super(context);
-
-        loadUrl(path);
-    }
-}
+//package com.example.broilr;
+//
+//import android.content.Context;
+//import android.webkit.WebView;
+//
+///**
+// * Created by anniedevine on 10/28/14.
+// */
+//
+//public class GifWebView extends WebView {
+//
+//    public GifWebView(Context context, String path) {
+//        super(context);
+//
+//        loadUrl(path);
+//    }
+//}
--- app/src/main/java/com/example/broilr/SplashScreen.java
+++ app/src/main/java/com/example/broilr/SplashScreen.java
@@ -2,7 +2,8 @@ package com.example.broilr;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.content.Intent;
+import android.os.Handler;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -11,19 +12,36 @@ import java.io.InputStream;
  */
 public class SplashScreen extends Activity {
 
-        /** Called when the activity is first created. */
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
 
-            InputStream stream = null;
-            try {
-                stream = getAssets().open("kissburger.png");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            GifWebView view = new GifWebView(this, "file:///android_asset    /kissburger.png");
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splashscreenfragment);
 
-            setContentView(view);
+        InputStream stream = null;
+        try {
+            stream = getAssets().open("kissburger.gif");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        GifWebView view = new GifWebView(this, "file:///android_asset    /kissburger.gif");
+
+        setContentView(view);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreen.this,MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
 }
