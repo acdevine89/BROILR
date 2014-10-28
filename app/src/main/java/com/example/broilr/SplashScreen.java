0e4a04c66a31915c73300550d8a1c23616cc5350
acdevine89 <annie.devine@detroitlabs.com>
2014-10-28 12:13:15 -0400

    Adding SplashScreen class for gif activity

---
+++ app/src/main/java/com/example/broilr/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.broilr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by anniedevine on 10/28/14.
+ */
+public class SplashScreen extends Activity {
+
+        /** Called when the activity is first created. */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            InputStream stream = null;
+            try {
+                stream = getAssets().open("kissburger.gif");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            GifWebView view = new GifWebView(this, "file:///android_asset    /kissburger.gif");
+
+            setContentView(view);
+        }
+}
